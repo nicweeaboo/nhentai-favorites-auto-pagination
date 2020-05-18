f4e32f4ff7be4b21a8ba7f3d183c1d41b9e03578
Nicweeaboo <61640166+nicweeaboo@users.noreply.github.com>
2020-05-17 23:50:25 -0300

    Update nhentai.rb

--- nhentai.rb
+++ nhentai.rb
@@ -1,28 +1,25 @@
-require 'watir-webdriver'
+require 'watir'
 
-browser = Watir::Browser.new :chrome, switches: ['--incognito']
-browser.goto 'https://nhentai.net/login/'
-browser.window.maximize
-sleep(1)
-browser.text_field(:name => 'username_or_email').set 'YOUR_USERNAME'
-browser.text_field(:name => 'password').set 'YOUR_PASSWORD'
-sleep(1)
-browser.button(:class => ['button', 'button-wide']).click
-
+pevert = Watir::Browser.new :firefox, switches: ['--incognito']
+pevert.goto 'https://nhentai.net/login/'
+pevert.window.maximize
+pevert.text_field(:name => 'username_or_email').set 'YOUR_USERNAME'
+pevert.text_field(:name => 'password').set 'YOUR_PASSWORD'
+pevert.button(:class => ['button', 'button-wide']).click
 while true
-    browser.a(:href => '/favorites/').click
+    pevert.a(:href => '/favorites/').click
     sleep(1)
-    browser.a(:href => '/favorites/random').click
+    pevert.a(:href => '/favorites/random').click
     sleep(1)
-    url = browser.url
-    pages = browser.as(:class => 'gallerythumb').count
+    url = pevert.url
+    pages = pevert.as(:class => 'gallerythumb').count
     1.upto(pages).each do |page|
-        browser.goto "#{url+page.to_s}"
-        height_page = browser.section(:id => 'image-container').height
+        pevert.goto "#{url+page.to_s}"
+        height_page = pevert.section(:id => 'image-container').height
         if height_page > 1080
             scroll_time = height_page / 5.2
             1.upto(scroll_time) do
-                browser.execute_script('window.scrollBy(0,2)')
+                pevert.execute_script('window.scrollBy(0,2)')
                 sleep(0.02)
             end
             sleep(0.5)
@@ -30,8 +27,7 @@ while true
             sleep(1)
         end
     end
-    browser.execute_script("window.alert('Last page. Picking anohter one...')")
+    pevert.execute_script("window.alert('Last page. Picking anohter one...')")
     sleep(3)
-    browser.alert.ok
+    pevert.alert.ok
 end
-
